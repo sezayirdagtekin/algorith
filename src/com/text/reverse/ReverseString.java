8b0e95078d0808dabeb2ec67f7176ee185281836
Sezayir Dağtekin <sezayir@hotmail.com>
2021-11-24 17:02:37 +0300

    reverse words in a sentence

--- src/com/text/reverse/ReverseString.java
+++ src/com/text/reverse/ReverseString.java
@@ -1,16 +1,23 @@
 package com.text.reverse;
 
+import java.util.StringJoiner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
-
-		System.out.println(reverse("sezayir"));
+	
 		System.out.println(reverse(""));
 		System.out.println(reverse(null));
-
-		System.out.println(reverseWithStringBuilder("java"));
+		System.out.println(reverse("sezayir"));
+		
 		System.out.println(reverseWithStringBuilder(""));
 		System.out.println(reverseWithStringBuilder(null));
+		System.out.println(reverseWithStringBuilder("java"));
+
+	
+		System.out.println(reverseEachWordInSentecence(""));
+		System.out.println(reverseEachWordInSentecence(null));
+		System.out.println(reverseEachWordInSentecence("Hi there! How are you?"));
 
 	}
 
@@ -30,9 +37,23 @@ public class ReverseString {
 		if (text == null || text.isEmpty()) {
 			return text;
 		}
-		
+
 		StringBuilder reverse = new StringBuilder(text);
 		return reverse.reverse().toString();
+	}
+
+	private static String reverseEachWordInSentecence(String sentence) {
+		if (sentence == null || sentence.isEmpty()) {
+			return sentence;
+		}
+		
+		StringJoiner joiner = new StringJoiner(" ");
+		String[] words = sentence.split(" ");
+		
+		for (String word : words) {
+			joiner.add(reverseWithStringBuilder(word));
+		}
+		return joiner.toString();
 	}
 
 }
